8e5db799597b675e79443e2b684e57a36801cfb2
nisarganavi <nisargaj23@gmail.com>
2022-12-22 14:34:21 +0530

    firstcommit

---
+++ src/com/LockedMe/Operations.java
@@ -0,0 +1,68 @@
+package com.LockedMe;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.TreeSet;
+
+public class Operations {
+	 public String createfile(String filename) {
+		 
+	        String folder="C:\\ProjectAssessment1";
+	        File f= new File(folder,filename);
+	        try {
+	            boolean result=f.createNewFile();
+	            if(result)
+	                return "Successfully created";
+	            else
+	                return "failed to create file";
+	        } catch (IOException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+	        return "failed to create";
+	    }
+	    
+	 
+	    public String deletefile(String filename) {
+	        String folder="C:\\ProjectAssessment1";
+	        File f= new File(folder,filename);
+	        try {
+	            boolean result=f.delete();
+	            if(result)
+	                return "Deleted successfully";
+	            else
+	                return "File Not Found";
+	            
+	        }catch(Exception e) {
+	            e.printStackTrace();
+	        }
+	        return "File Not Found";
+	    }
+	    
+	    
+	    
+	    public TreeSet<String> ascendingorder(){
+	        TreeSet<String> filenames=new TreeSet<String>();
+	        String directpath="C:\\ProjectAssessment1";
+	        File dirmain=new File(directpath);
+	        File filesarr[]=null;
+	        if(dirmain.exists() && dirmain.isDirectory()) {
+	            filesarr=dirmain.listFiles();
+	        }
+	        for(File f: filesarr) {
+	            filenames.add(f.getName());
+	        }
+	        return filenames;
+	    }
+	    
+	    
+	    
+	    public boolean search(String filename) {
+	        TreeSet<String> filenames=this.ascendingorder();
+	        for(String i: filenames) {
+	            if(i.equals(filename))
+	                return true;
+	        }
+	        return false; 
+	    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nisjayar
+ *
+ */
+module LockedMe {
+}
